37fc156765d0c408ea9d5bb8a87e9a55bfdfd285
kathleenbk <katie.kava3@gmail.com>
2022-08-22 19:00:15 -0800

    initcommit

---
+++ src/com/kavan/sll/SLLTester.java
@@ -0,0 +1,15 @@
+package com.kavan.sll;
+
+public class SLLTester {
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+
+        sll.AddBack(1);
+        sll.AddBack(2);
+        sll.AddBack(3);
+        sll.AddBack(4);
+
+        int val = sll.removeBack();
+        System.out.println(val);
+    }
+}
